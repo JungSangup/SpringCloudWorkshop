d3b1e3c7e0dbf7e66ba1c680b1b53a5116c4ee64
myminseok <myminseok@gmail.com>
2017-11-24 13:58:05 +0900

    session_03/lab_05, upgrade spring boot, apply lombok rename the project

--- session_03/lab_05/complete/cities/src/main/java/org/example/cities/domain/City.java
+++ session_03/lab_05/complete/cities/src/main/java/org/example/cities/domain/City.java
@@ -1,10 +1,17 @@
 package org.example.cities.domain;
 
-import javax.persistence.*;
 import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="city")
+@lombok.Getter
+@lombok.Setter
 public class City implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -30,31 +37,5 @@ public class City implements Serializable {
     @Column
     private String longitude;
 
-    public String getName() { return name; }
-
-    public void setName(String name) { this.name = name; }
-
-    public String getPostalCode() { return postalCode; }
-
-    public void setPostalCode(String postalCode) { this.postalCode = postalCode; }
-
-    public long getId() { return id; }
-
-    public void setId(long id) { this.id = id; }
-
-    public String getStateCode() { return stateCode; }
-
-    public void setStateCode(String stateCode) { this.stateCode = stateCode; }
-
-    public String getCounty() { return county; }
-
-    public void setCounty(String county) { this.county = county; }
-
-    public String getLatitude() { return latitude; }
-
-    public void setLatitude(String latitude) { this.latitude = latitude; }
-
-    public String getLongitude() { return longitude; }
-
-    public void setLongitude(String longitude) { this.longitude = longitude; }
+    
 }
--- session_03/lab_05/complete/cities/src/main/java/org/example/cities/repositories/CityRepository.java
+++ session_03/lab_05/complete/cities/src/main/java/org/example/cities/repositories/CityRepository.java
@@ -3,22 +3,23 @@ package org.example.cities.repositories;
 import org.example.cities.domain.City;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource(collectionResourceRel = "cities", path = "cities")
+//@RepositoryRestResource(collectionResourceRel = "cities", path = "cities")
+@Repository
 public interface CityRepository extends PagingAndSortingRepository<City, Long> {
-    @RestResource(path = "name", rel = "name")
+    //@RestResource(path = "name", rel = "name")
     Page<City> findByNameIgnoreCase(@Param("q") String name, Pageable pageable);
-
-    @RestResource(path = "nameContains", rel = "nameContains")
+    //@RestResource(path = "nameContains", rel = "nameContains")
     Page<City> findByNameContainsIgnoreCase(@Param("q") String name, Pageable pageable);
 
-    @RestResource(path = "state", rel = "state")
+    //@RestResource(path = "state", rel = "state")
     Page<City> findByStateCodeIgnoreCase(@Param("q") String stateCode, Pageable pageable);
 
-    @RestResource(path = "postalCode", rel = "postalCode")
+    //@RestResource(path = "postalCode", rel = "postalCode")
     Page<City> findByPostalCode(@Param("q") String postalCode, Pageable pageable);
 }
