132da684e1d6d55d94fc79001307251c380e24c7
myminseok <myminseok@gmail.com>
2017-11-26 02:22:31 +0900

    modify code for PWS

--- session_05/lab_08/hello-world/hello-ui/src/main/java/io/pivotal/helloui/controller/SayHelloCommand.java
+++ session_05/lab_08/hello-world/hello-ui/src/main/java/io/pivotal/helloui/controller/SayHelloCommand.java
@@ -1,26 +1,32 @@
 package io.pivotal.helloui.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import io.pivotal.helloui.domain.HelloResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class SayHelloCommand {
-    @Autowired
-    @LoadBalanced
-    RestTemplate restTemplate;
+	
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 
+	@Autowired
+	RestTemplate restTemplate;
+
+    
 
     @HystrixCommand(fallbackMethod = "sayHelloFallback")
     public String sayHello(String toWho) {
         try{
-            //return restTemplate.getForObject("https://hello-world-server/hello?name={name}", String.class, toWho);
-        	return restTemplate.getForObject("hello-world-server/hello?name={name}", String.class, toWho);
+            return restTemplate.getForObject("http://hello-world-server/hello?name={name}", String.class, toWho);
+        	//return restTemplate.getForObject("hello-world-server/hello?name={name}", String.class, toWho);
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
